58d7568fc1a117b2776e85f440005ee280718210
coothon <metalbender@outlook.com.au>
2023-03-23 11:43:18 +0800

    Add dynarray_resize_zero_rest().

--- src/dynarray.h
+++ src/dynarray.h
@@ -37,10 +37,12 @@ enum dynarray_offsets {
 void *_dynarray_new(size_t item_size, size_t min_alloc);
 
 // Provide the array and amount of items needed. Returns NULL on failure.
-void *dynarray_resize(void *dynarray, size_t new_size);
+void *dynarray_resize(void *dynarray, size_t new_amount);
+void *dynarray_resize_zero_rest(void *dynarray, size_t new_amount);
 
 // Sets all elements to c. Only supports ints, unfortunately.
 void dynarray_set(void *dynarray, int c);
+void _dynarray_set(void *dynarray, int c, size_t allocated, size_t item_size);
 
 #endif // DYNARRAY_H_
 
@@ -73,7 +75,7 @@ void *dynarray_resize(void *dynarray, size_t new_amount) {
 		return NULL;
 
 	void *new_address = realloc(DYNARRAY_TRUE_START(dynarray), (DYNARRAY_TRUE_START_OFFSET * sizeof(size_t)) +
-																   (DYNARRAY_ITEM_SIZE(dynarray) * new_amount));
+	                                                               (DYNARRAY_ITEM_SIZE(dynarray) * new_amount));
 	if (!new_address)
 		return NULL;
 
@@ -85,14 +87,49 @@ void *dynarray_resize(void *dynarray, size_t new_amount) {
 	return new_address;
 }
 
-void dynarray_set(void *dynarray, int c) {
+void *dynarray_resize_zero_rest(void *dynarray, size_t new_amount) {
+	if (!dynarray)
+		return NULL;
+
+	size_t old_amount = DYNARRAY_ALLOCATED(dynarray);
+	size_t item_size = DYNARRAY_ITEM_SIZE(dynarray);
+	if (old_amount >= new_amount)
+		old_amount = 0;
+
+	void *new_address = realloc(DYNARRAY_TRUE_START(dynarray),
+	                            (DYNARRAY_TRUE_START_OFFSET * sizeof(size_t)) + (item_size * new_amount));
+	if (!new_address)
+		return NULL;
+
+	// Set back to user's data.
+	new_address = (void *)(((size_t *)new_address) + DYNARRAY_TRUE_START_OFFSET);
+
+	// Zero the newly allocated part.
+	if (old_amount != 0) {
+		void *zero_from = (void *)(((char *)new_address) + (old_amount * item_size));
+		_dynarray_set(zero_from, 0, new_amount - old_amount, item_size);
+	}
+
+	DYNARRAY_ALLOCATED(new_address) = new_amount;
+
+	return new_address;
+}
+
+void dynarray_set(void *dynarray, int c) { _dynarray_set(dynarray, c, 0, 0); }
+
+void _dynarray_set(void *dynarray, int c, size_t allocated, size_t item_size) {
 	if (!dynarray)
 		return;
 
-	size_t amount = DYNARRAY_ALLOCATED(dynarray);
-	size_t size = DYNARRAY_ITEM_SIZE(dynarray);
+	if (item_size == 0) {
+		item_size = DYNARRAY_ITEM_SIZE(dynarray);
+	}
 
-	memset(dynarray, c, amount * size);
+	if (allocated == 0) {
+		allocated = DYNARRAY_ALLOCATED(dynarray);
+	}
+
+	memset(dynarray, c, allocated * item_size);
 }
 
 #undef DYNARRAY_IMPLEMENTATION
